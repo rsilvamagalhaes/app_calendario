c11bbe520ea0b36286a004c2abb426a360f16849
Rodrigo S Magalhaes <rsilvamagalhaes@gmail.com>
2014-01-30 18:46:26 -0200

    Link calendario angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,13 +6,12 @@ angular.module('appCalendarioApp', [
   'ngSanitize',
   'ngRoute'
 ])
-  .config(function ($routeProvider) {
+
+.config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
+      .when('/', {templateUrl: 'views/main.html', controller: 'MainCtrl'})
+      .when('/calendario', {templateUrl: 'views/calendario.html', controller: 'calendarController'})
       .otherwise({
         redirectTo: '/'
       });
-  });
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('appCalendarioApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', $function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+});
