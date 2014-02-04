1c625f7bcca6f6b8be727ada7e37e7f0362e787a
Rodrigo S Magalhaes <rsilvamagalhaes@gmail.com>
2014-02-03 23:35:53 -0200

    Ajustes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,11 @@
 'use strict';
 
-angular.module('appCalendarioApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
+angular.module('appCalendarioApp', ['ngCookies', 'ngResource', 'ngSanitize',  'ngRoute'])
 
 .config(function ($routeProvider) {
     $routeProvider
       .when('/', {templateUrl: 'views/main.html', controller: 'MainCtrl'})
-      .when('/calendario', {templateUrl: 'views/calendario.html', controller: 'calendarController'})
+      .when('/calendario', {templateUrl: 'views/calendario.html', controller: 'calendarioController'})
       .otherwise({
         redirectTo: '/'
       });
--- app/scripts/controllers/calendarioController.js
+++ app/scripts/controllers/calendarioController.js
@@ -1,3 +1,4 @@
 'use strict';
-angular.module('appCalendarioApp').controller('MainCtrl', $function ($scope) {
-});
+angular.module('appCalendarioApp').controller('calendarioController', ['$scope', function ($scope) {
+    $scope.mes = "Janeiro";
+}]);
