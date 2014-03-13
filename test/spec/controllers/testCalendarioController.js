7ee609fac9dd0ea428f5afedb30f3300dc0fe88e
Rodrigo S Magalhaes <rsilvamagalhaes@gmail.com>
2014-03-12 22:28:50 -0300

    Teste calendario

---
+++ test/spec/controllers/testCalendarioController.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: calendarioController', function () {
+
+  // load the controller's module
+  beforeEach(module('appCalendarioApp'));
+
+  var calendarioController,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    calendarioController = $controller('calendarioController', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(scope.mes.length).toBe(12);
+  });
+});
