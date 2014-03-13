15f5b962d99a72359966f00e4df8fcaca8cda58a
Rodrigo S Magalhaes <rsilvamagalhaes@gmail.com>
2014-03-12 22:28:27 -0300

    Configuracao Karma teste

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -5,14 +5,13 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('appCalendarioApp'));
 
-  var MainCtrl,
-    scope;
+  var MainCtrl, scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+        $scope: scope
     });
   }));
 
